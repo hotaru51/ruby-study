11a9890ae23b2087aec5f8d6184ae9f2af386e94
hotaru <36354847+hotaru51@users.noreply.github.com>
2023-05-28 22:36:16 +0900

    open_omakeを2回以上実行した場合のメッセージを変更

--- ruby_sinatra_web_app/chapter3/15/kodomocurry.rb
+++ ruby_sinatra_web_app/chapter3/15/kodomocurry.rb
@@ -23,6 +23,7 @@ class KodomoCurry < Curry
   def initialize(karasa = 1, ryou = 200)
     set_karasa(karasa)
     set_ryou(ryou)
+    @opened = false
   end
 
   def set_karasa(karasa)
@@ -32,12 +33,19 @@ class KodomoCurry < Curry
   end
 
   def open_omake
-    puts 'Open omake!'
+    if !@opened
+      puts 'Open omake!'
+      @opened = true
+    else
+      puts 'mounaiyo!'
+    end
   end
 end
 
 iguano = Curry.new(5)
-setgo = KodomoCurry.new
+# KodomoCurryはkarasaを2以上にするとエラー
+# setgo = KodomoCurry.new(2, 200)
+setgo = KodomoCurry.new(1, 200)
 
 iguano.set_karasa(99)
 
@@ -48,4 +56,5 @@ puts " ryou = #{iguano.ryou}"
 puts 'setgo'
 puts " karasa = #{setgo.karasa}"
 puts " ryou = #{setgo.ryou}"
+setgo.open_omake
 setgo.open_omake
