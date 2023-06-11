9911275b6b3d9908b07c21a67524231d3a3c93d5
hotaru <36354847+hotaru51@users.noreply.github.com>
2023-06-11 17:37:15 +0900

    5.3.2 DBの作成

---
+++ ruby_sinatra_web_app/chapter5/BBSapp/dbinit.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bbsdata (
+  id    integer PRIMARY KEY,
+  name  varchar(30),
+  entry varchar(150)
+);
+
+INSERT INTO bbsdata values (1, 'You Watanabe', 'zensoku zenshin yo-soro-');
+INSERT INTO bbsdata values (2, 'Kotori Minami', '(・８・)');
