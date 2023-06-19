6070d02b52f0964c17e47acb73074a70b2f3c831
hotaru <36354847+hotaru51@users.noreply.github.com>
2023-06-19 23:46:45 +0900

    命名規則に従ったテーブル名の場合、table_nameの指定が不要

--- ruby_sinatra_web_app/chapter5/BBSapp/dbinit.sql
+++ ruby_sinatra_web_app/chapter5/BBSapp/dbinit.sql
@@ -6,3 +6,12 @@ CREATE TABLE bbsdata (
 
 INSERT INTO bbsdata values (1, 'You Watanabe', 'zensoku zenshin yo-soro-');
 INSERT INTO bbsdata values (2, 'Kotori Minami', '(・８・)');
+
+CREATE TABLE members (
+  id   integer PRIMARY KEY,
+  name varchar(30)
+);
+
+INSERT INTO members (id, name) VALUES
+  (1, 'Nico Yazawa'),
+  (2, 'Maki, Nishikino');
--- ruby_sinatra_web_app/chapter5/BBSapp/dbtest.rb
+++ ruby_sinatra_web_app/chapter5/BBSapp/dbtest.rb
@@ -9,6 +9,11 @@ class BBSdata < ActiveRecord::Base
   self.table_name = 'bbsdata'
 end
 
+class Member < ActiveRecord::Base
+end
+
 a = BBSdata.find(1)
+b = Member.find(1)
 
 puts "#{a.id}, #{a.name}, #{a.entry}"
+puts "#{b.id}, #{b.name}"
